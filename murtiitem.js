6b9d6773022cb745733132ebb4b4d58cee967bab
Nikita Jaiswal <135646609+nikitajaiswalgithub@users.noreply.github.com>
2023-08-28 21:18:53 +0530

    added murtiitem.js

---
+++ murtiitem.js
@@ -0,0 +1,414 @@
+const murtiItem= [
+    {
+    id: 1,
+    name: 'bholenath',
+    category : 'bholenath',
+    rating : 4.3,
+    price: 13,
+    img: '/images/image/bholenath/bhole1.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'bholenath',
+    category : 'bholenath',
+    rating : 4.3,
+    price: 15,
+    img: '/images/image/bholenath/bole2.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'bholenath',
+    category : 'bholenath',
+    rating : 4.3,
+    price: 18,
+    img: '/images/image/bholenath/bole3.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'bholenath',
+    category : 'bholenath',
+    rating : 4.3,
+    price: 15,
+    img: '/images/image/bholenath/bole4.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'bholenath',
+    category : 'bholenath',
+    rating : 4.3,
+    price: 10,
+    img: '/images/image/bholenath/bole5.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'bholenath',
+    category : 'bholenath',
+    rating : 4.3,
+    price: 12,
+    img: '/images/image/bholenath/bole6.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'bholenath',
+    category : 'bholenath',
+    rating : 4.3,
+    price: 13,
+    img: '/images/image/bholenath/bole7.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'bholenath',
+    category : 'bholenath',
+    rating : 4.3,
+    price: 20,
+    img: '/images/image/bholenath/bole8.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'bholenath',
+    category : 'bholenath',
+    rating : 4.3,
+    price: 15,
+    img: '/images/image/bholenath/bole9.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 10,
+    name: 'bramhaji',
+    category : 'bramhaji',
+    rating : 4.3,
+    price: 11,
+    img: '/images/image/bramhaji/bramha1.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'bramhaji',
+    category : 'bramhaji',
+    rating : 4.3,
+    price: 10,
+    img: '/images/image/bramhaji/bramha2.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'bramhaji',
+    category : 'bramhaji',
+    rating : 4.3,
+    price: 14,
+    img: '/images/image/bramhaji/bramha3.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'bramhaji',
+    category : 'bramhaji',
+    rating : 4.3,
+    price: 12,
+    img: '/images/image/bramhaji/bramha4.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'bramhaji',
+    category : 'bramhaji',
+    rating : 4.3,
+    price: 17,
+    img: '/images/image/bramhaji/bramha1.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'bramhaji',
+    category : 'bramhaji',
+    rating : 4.3,
+    price: 20,
+    img: '/images/image/bramhaji/bramha2.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 16,
+    name: 'diye',
+    category : 'diye',
+    rating : 4.3,
+    price: 15,
+    img: 'images/image/diye/diya10.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'diye',
+    category : 'diye',
+    rating : 4.3,
+    price: 10,
+    img: 'images/image/diye/diya11.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'diye',
+    category : 'diye',
+    rating : 4.3,
+    price: 15,
+    img: 'images/image/diye/diya12.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'diye',
+    category : 'diye',
+    rating : 4.3,
+    price: 12,
+    img: 'images/image/diye/diya13.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'diye',
+    category : 'diye',
+    rating : 4.3,
+    price: 8,
+    img: 'images/image/diye/diya14.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'diye',
+    category : 'diye',
+    rating : 4.3,
+    price: 7,
+    img: 'images/image/diye/diya17.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'diye',
+    age: '32',
+    category : 'diye',
+    rating : 4.3,
+    price: 5,
+    img: 'images/image/diye/diya16.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 23,
+    name: 'durgam',
+    category : 'durgam',
+    rating : 4.3,
+    price: 8,
+    img: 'images/image/durgam/durga1.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'durgam',
+    category : 'durgam',
+    rating : 4.3,
+    price: 7,
+    img: 'images/image/durgam/durga2.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'durgam',
+    category : 'durgam',
+    rating : 4.3,
+    price: 5,
+    img: 'images/image/durgam/durga3.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'durgam',
+    category : 'durgam',
+    rating : 4.3,
+    price: 7,
+    img: 'images/image/durgam/durga4.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'durgam',
+    category : 'durgam',
+    rating : 4.3,
+    price: 10,
+    img: 'images/image/durgam/durga5.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'durgam',
+    category : 'durgam',
+    rating : 4.3,
+    price: 4,
+    img: 'images/image/durgam/durga6.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'durgam',
+    category : 'durgam',
+    rating : 4.3,
+    price: 4,
+    img: 'images/image/durgam/durga7.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 30,
+    name: 'ganesh',
+    category : 'ganesh',
+    rating : 4.3,
+    price: 8,
+    img: 'images/image/ganesh/genesh1.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'ganesh',
+    category : 'ganesh',
+    rating : 4.3,
+    price: 7,
+    img: 'images/image/ganesh/genesh2.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'ganesh',
+    category : 'ganesh',
+    rating : 4.3,
+    price: 5,
+    img: 'images/image/ganesh/genesh3.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'ganesh',
+    category : 'ganesh',
+    rating : 4.3,
+    price: 16,
+    img: 'images/image/ganesh/genesh4.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'ganesh',
+    category : 'ganesh',
+    rating : 4.3,
+    price: 14,
+    img: 'images/image/ganesh/genesh5.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'ganesh',
+    category : 'ganesh',
+    rating : 4.3,
+    price: 10,
+    img: 'images/image/ganesh/genesh6.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'ganesh',
+    category : 'ganesh',
+    rating : 4.3,
+    price: 8,
+    img: 'images/image/ganesh/genesh7.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 37,
+    name: 'glass',
+    category : 'glass',
+    rating : 4.3,
+    price: 18,
+    img: 'images/image/glass/glass1.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'glass',
+    category : 'glass',
+    rating : 4.3,
+    price: 20,
+    img: 'images/image/glass/glass2.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'glass',
+    category : 'glass',
+    rating : 4.3,
+    price: 12,
+    img: 'images/image/glass/glass3.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'glass',
+    category : 'glass',
+    rating : 4.3,
+    price: 10,
+    img: 'images/image/glass/glass4.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'glass',
+    category : 'glass',
+    rating : 4.3,
+    price: 15,
+    img: 'images/image/glass/glass5.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'glass',
+    category : 'glass',
+    rating : 4.3,
+    price: 40,
+    img: 'images/image/glass/glass6.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'glass',
+    category : 'glass',
+    rating : 4.3,
+    price: 25,
+    img: 'images/image/glass/glass7.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'glass',
+    category : 'glass',
+    rating : 4.3,
+    price: 34,
+    img: 'images/image/glass/glass8.jpg',
+    quantity: 1
+},
+]
+
+export {murtiItem};
